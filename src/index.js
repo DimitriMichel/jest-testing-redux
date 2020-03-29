219765ad650b8a8f094d9f6d4000fc5f3364cd98
Dimitri PL <dimitrimachines@gmail.com>
2020-03-28 22:48:37 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./sass/main.sass"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './redux/reducers';
+import './sass/main.sass';
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
+/*ReactDOM.render(
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);*/
