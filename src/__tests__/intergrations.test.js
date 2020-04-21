23e09eb1c7c7b0b84aa71b11ae1f753daa5b5500
Dimitri PL <dimitrimachines@gmail.com>
2020-04-20 23:35:47 -0400

    fetch mocks

--- src/__tests__/intergrations.test.js
+++ src/__tests__/intergrations.test.js
@@ -3,12 +3,24 @@ import { mount } from 'enzyme';
 import Root from '../Root';
 import App from '../components/App';
 
-const wrapped = mount(
-  <Root>
-    <App />
-  </Root>
-);
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+it('should call api and return values', function () {
+  fetch('http://jsonplaceholder.typicode.com/comments')
+    .then((response) => response.json())
+    .then((data) => {
+      expect(data).toEqual([{ name: 'Fetched#1' }]);
+    })
+    .catch((err) => console.log(err));
+});
 it('should fetch a list of comments and display them', function () {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
   wrapped.find('.fetch-comments').simulate('click');
 
   expect(wrapped.find('li').length).toEqual(500);
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,5 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+require('jest-fetch-mock').enableMocks();
